d433b2aa2a443f78ea21100e7eef9fd1ba8f4715
jmarah <Jmarah211@gmail.com>
2019-02-02 16:00:22 -0500

    Add files via upload

---
+++ networks-project-1-master/networks-project-1-master/src/client.c
@@ -0,0 +1,67 @@
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*
+ * James Marah
+ * Group 7
+ * Client - C Version
+ */
+void diep(char *s)
+{
+	perror(s);
+	exit(1);
+}
+void setupClient()
+{
+	struct sockaddr_in serverInfo;
+    int clientSocket;
+
+    if ((clientSocket = socket(PF_INET,SOCK_STREAM,IPPROTO_TCP)) < 0)
+    {
+    	diep("socket() failed");
+    }
+
+    // Fill in the information for the server I want to connect to.
+    serverInfo.sin_family = AF_INET;
+    serverInfo.sin_addr.s_addr = inet_addr("127.0.0.1");
+    serverInfo.sin_port = htons(4446);
+
+    // Actually try and connect to the server.
+    if (connect(clientSocket, (struct sockaddr*)&serverInfo,sizeof(serverInfo)) < 0){
+	diep("connect() failed");}
+	
+	puts("Type a 10 character message below:");
+	
+    char data[11];
+	scanf("%s", data);
+	
+    int length = strlen(data);
+    if (send(clientSocket,data,length,0) != length)
+		{
+			diep("send() sent a different number of bytes than expected.");
+		}
+	
+	puts("Sent message to server");
+
+	char datab[11];
+	if (recv(clientSocket,datab,11,0) < 0)
+		{
+			diep("recv() got a different number of bytes than expected.");
+		}
+	
+	printf("Received Message from Server: %s\n", datab);
+
+    close(clientSocket);
+}
+int main()
+{
+	setupClient();
+
+	return 1;
+}
